af4739f99a6a8b81f531f6b73a6c8f35af40d540
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-06 13:03:28 -0300

    Remove static font-face

--- src/styles/base/global/index.js
+++ src/styles/base/global/index.js
@@ -3,41 +3,6 @@ import { defaultTheme } from '../theme';
 
 export const GlobalStyle = createGlobalStyle`
 
-  @font-face {
-    font-family: 'Inter';
-    src: url('/src/assets/fonts/Inter-SemiBold.ttf');
-    font-style: normal;
-    font-weight: 600;
-  }
-  
-  @font-face {
-    font-family: 'Inter';
-    src: url('/src/assets/fonts/Inter-Regular.ttf');
-    font-style: normal;
-    font-weight: 400;
-  }
-
-  @font-face {
-    font-family: 'Inter';
-    src: url('/src/assets/fonts/Inter-ExtraBold.ttf');
-    font-style: normal;
-    font-weight: 800;
-  }
-
-  @font-face {
-    font-family: 'Inter';
-    src: url('/src/assets/fonts/Inter-Bold.ttf');
-    font-style: normal;
-    font-weight: 700;
-  }
-
-  @font-face {
-    font-family: 'Fira Mono';
-    src: url('/src/assets/fonts/FiraMono-Regular.ttf');
-    font-style: normal;
-    font-weight: 400;
-  }
-
   ::-webkit-scrollbar {
     width: 14px;
   }
@@ -76,6 +41,6 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   .animate__zoomIn {
-   --animate-duration: 0.25s;
+    --animate-duration: 0.25s;
   }
 `;
