9d96a17e5612615f6ceac550b99fe11c46fe62c5
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-11-06 12:09:11 -0300

    Add / to font face url

--- src/styles/base/global/index.js
+++ src/styles/base/global/index.js
@@ -5,35 +5,35 @@ export const GlobalStyle = createGlobalStyle`
 
   @font-face {
     font-family: 'Inter';
-    src: url('src/assets/fonts/Inter-SemiBold.ttf');
+    src: url('/src/assets/fonts/Inter-SemiBold.ttf');
     font-style: normal;
     font-weight: 600;
   }
   
   @font-face {
     font-family: 'Inter';
-    src: url('src/assets/fonts/Inter-Regular.ttf');
+    src: url('/src/assets/fonts/Inter-Regular.ttf');
     font-style: normal;
     font-weight: 400;
   }
 
   @font-face {
     font-family: 'Inter';
-    src: url('src/assets/fonts/Inter-ExtraBold.ttf');
+    src: url('/src/assets/fonts/Inter-ExtraBold.ttf');
     font-style: normal;
     font-weight: 800;
   }
 
   @font-face {
     font-family: 'Inter';
-    src: url('src/assets/fonts/Inter-Bold.ttf');
+    src: url('/src/assets/fonts/Inter-Bold.ttf');
     font-style: normal;
     font-weight: 700;
   }
 
   @font-face {
     font-family: 'Fira Mono';
-    src: url('src/assets/fonts/FiraMono-Regular.ttf');
+    src: url('/src/assets/fonts/FiraMono-Regular.ttf');
     font-style: normal;
     font-weight: 400;
   }
